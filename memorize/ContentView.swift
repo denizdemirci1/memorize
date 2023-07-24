5221fdc1809884a93dac987d6d4f475a10871ea5
deniz demirci <deniz.demirci@global.com>
2023-07-24 09:44:44 +0100

    Initial commit

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -9,18 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+struct CardView: View {
+    // @State doesn't make the bool value changable. It makes the variable a pointer. The pointer doesn't change. The value that it points to change.
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
