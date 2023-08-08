127665d17d5c89970433a0a0f1c6a837753bfa8b
deniz demirci <deniz.demirci@global.com>
2023-08-08 09:29:15 +0100

    Lecture 2 - More SwiftUI

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,28 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚚", "🚛", "🚜", "🛴", "🚲", "🛵", "✈️", "🚀", "🚇", "🚂", "🚢"]
+    @State var emojiCount = 20
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
 struct CardView: View {
     // @State doesn't make the bool value changable. It makes the variable a pointer. The pointer doesn't change. The value that it points to change.
     @State var isFaceUp: Bool = true
+    var content: String
+    
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
-                Text("✈️").font(.largeTitle)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
