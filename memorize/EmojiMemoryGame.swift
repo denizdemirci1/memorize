1b60c16cf308cd880bb46c3c4b64bcbf5721b1b0
deniz demirci <deniz.demirci@global.com>
2023-08-21 15:05:41 +0100

    Computed property, extension, functional programming,

--- memorize/EmojiMemoryGame.swift
+++ memorize/EmojiMemoryGame.swift
@@ -9,26 +9,28 @@ import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
     
+    typealias Card = MemoryGame<String>.Card
+    
     // static makes this global variable: EmojiMemoryGame.emojis
     // it's not an instance of EmojiMemoryGame and not all instances get an emojis.
     // it's only created once and attached to EmojiMemoryGame itself, not to its instances, such as cards.
-    static let emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚚", "🚛", "🚜", "🛴", "🚲", "🛵", "✈️", "🚀", "🚇", "🚂", "🚢"]
+    private static let emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚚", "🚛", "🚜", "🛴", "🚲", "🛵", "✈️", "🚀", "🚇", "🚂", "🚢"]
     
-    static func createMemoryGame() -> MemoryGame<String> {
+    private static func createMemoryGame() -> MemoryGame<String> {
         MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
             emojis[pairIndex]
         }
     }
     
-    @Published private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model = createMemoryGame()
     
-    var cards: Array<MemoryGame<String>.Card> {
+    var cards: Array<Card> {
         model.cards
     }
     
     // MARK: - Intent(s)
     
-    func choose(_ card: MemoryGame<String>.Card) {
+    func choose(_ card: Card) {
         model.choose(card)
     }
 }
