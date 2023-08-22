2df518f926248b961c06b9f082873ed836c80c2e
deniz demirci <deniz.demirci@global.com>
2023-08-22 13:31:19 +0100

    SwiftUI Layout and Geometry Reader

---
+++ memorize/notes/LectureFiveNotes.swift
@@ -0,0 +1,14 @@
+//
+//  LectureFiveNotes.swift
+//  memorize
+//
+//  Created by Deniz Demirci on 21/08/2023.
+//
+
+/**
+ Layout - How is the screen is apportioned to the views?
+    1- Container view "offers'" space to the views inside of them
+    2- Views choose  what size they want to be
+    3- Container then positions the views
+    4- Then container view chooses its own size
+ */
